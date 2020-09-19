4867470055d0eb729d15252e2a11264ed9885291
Kishan Patel <kishanpatel1892@gmail.com>
2020-09-19 14:06:14 -0400

    Implement Shell Sort

---
+++ src/ShellSort.java
@@ -0,0 +1,23 @@
+public class ShellSort {
+    public static void main(String[] args) {
+
+        int[] arrayList = {99, 55, 1, 3, -22, 15, -15};
+
+        for (int gap = arrayList.length / 2; gap > 0; gap--) {
+            for (int i = gap; i < arrayList.length; i++) {
+                int element = arrayList[i];
+                int j = i;
+                while (j >= gap && arrayList[j - gap] > element) {
+                    arrayList[j] = arrayList[j - gap];
+                    j -= gap;
+                }
+                arrayList[j] = element;
+            }
+        }
+
+        for (int i = 0; i < arrayList.length - 1; i++) {
+            System.out.println(arrayList[i]);
+        }
+
+    }
+}
